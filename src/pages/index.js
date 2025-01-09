1b39248e5f1a0591be14ba34f2739ef5c6686162
GenLabIB <henrich.genlabib@gmail.com>
2025-01-09 18:02:08 +0530

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,12 +6,18 @@ import Head from 'next/head'
 const Alo = () => {
   return (
     <Fragment>
-         <Head>
-        <title>Empowering the Next Generation</title>
+      <Head>
+        <title>GenLab IB - Empowering the next generation through our dynamic cohort programs</title>
         <meta
           name="description"
-          content="Join GenLab IB to explore and develop your skills in a collaborative environment."
+          content="Join GenLab IB. Students Program. For aspiring innovators looking to explore and develop their skills in a collaborative environment."
         />
+        <meta property="og:title" content="GenLab IB - Empowering the next generation through our dynamic cohort programs" />
+        <meta property="og:description" content="Join GenLab IB. Students Program. For aspiring innovators looking to explore and develop their skills in a collaborative environment." />
+        <meta property="og:url" content="https://www.genlabib.in" />
+        <meta property="og:site_name" content="GenLab IB" />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:type" content="website" />
       </Head>
       <Home />
     </Fragment>
@@ -19,3 +25,4 @@ const Alo = () => {
 }
 
 export default Alo
+
